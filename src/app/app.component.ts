9942dce1d650e921245f994ebb4fe781a2f42463
Jared Youtsey <github@fivedice.net>
2019-06-25 13:30:24 -0500

    move to angular 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,43 +3,40 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MaterialThemeGenerator';
+	title = 'MaterialThemeGenerator';
 
-  constructor(
-    private zone: NgZone,
-    private matIconRegistry: MatIconRegistry,
-    domSanitizer: DomSanitizer
-  ) {
-    let icons = 'Filled';
-    this.matIconRegistry.addSvgIconSet(
-      domSanitizer.bypassSecurityTrustResourceUrl(`/assets/material-icons-${icons}.svg`));
+	constructor(private zone: NgZone, private matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
+		let icons = 'Filled';
+		this.matIconRegistry.addSvgIconSet(
+			domSanitizer.bypassSecurityTrustResourceUrl(`/assets/material-icons-${icons}.svg`)
+		);
 
-    this.matIconRegistry.addSvgIcon('github',
-      domSanitizer.bypassSecurityTrustResourceUrl('/assets/github.svg'));
+		this.matIconRegistry.addSvgIcon('github', domSanitizer.bypassSecurityTrustResourceUrl('/assets/github.svg'));
 
-    if (window.parent !== window) {
-      console.log('Watching icons');
+		if (window.parent !== window) {
+			console.log('Watching icons');
 
-      window.parent.addEventListener('message', (ev: MessageEvent) => {
-        if (ev.data.icons) {
-          this.zone.run(() => {
-            const val = ev.data.icons;
-            if (val !== icons) {
-              console.log('Icons Changed', val);
-              icons = val;
-              this.matIconRegistry['_iconSetConfigs'].set('', null);
-              this.matIconRegistry.addSvgIconSet(
-                domSanitizer.bypassSecurityTrustResourceUrl(`/assets/material-icons-${icons}.svg`));
-            }
-            window.parent.postMessage('Done', window.location.toString());
-          });
-        }
-      });
-    }
-  }
+			window.parent.addEventListener('message', (ev: MessageEvent) => {
+				if (ev.data.icons) {
+					this.zone.run(() => {
+						const val = ev.data.icons;
+						if (val !== icons) {
+							console.log('Icons Changed', val);
+							icons = val;
+							this.matIconRegistry['_iconSetConfigs'].set('', null);
+							this.matIconRegistry.addSvgIconSet(
+								domSanitizer.bypassSecurityTrustResourceUrl(`/assets/material-icons-${icons}.svg`)
+							);
+						}
+						window.parent.postMessage('Done', window.location.toString());
+					});
+				}
+			});
+		}
+	}
 }
--- src/app/theme-builder/theme-builder.component.ts
+++ src/app/theme-builder/theme-builder.component.ts
@@ -10,127 +10,131 @@ import { ThemeService, Theme } from '../theme.service';
 
 import { highlight } from './highlight';
 
-
 @Component({
-  selector: 'app-theme-builder',
-  templateUrl: './theme-builder.component.html',
-  styleUrls: ['./theme-builder.component.scss']
+	selector: 'app-theme-builder',
+	templateUrl: './theme-builder.component.html',
+	styleUrls: ['./theme-builder.component.scss']
 })
 export class ThemeBuilderComponent implements OnInit {
+	form: FormGroup;
 
-  form: FormGroup;
+	refresh: Subject<number> = new Subject();
+	ready: Subject<boolean> = new Subject();
+	isReady: boolean;
+	showingSource = false;
+	source = '';
+	css = '';
+	sourcePretty = '';
+	first = true;
 
-  refresh: Subject<number> = new Subject();
-  ready: Subject<boolean> = new Subject();
-  isReady: boolean;
-  showingSource = false;
-  source = '';
-  css = '';
-  sourcePretty = '';
-  first = true;
+	constructor(
+		private el: ElementRef,
+		private zone: NgZone,
+		private snackbar: MatSnackBar,
+		private dialog: MatDialog,
+		public service: ThemeService
+	) {
+		if (window.location.search) {
+			setTimeout(() => {
+				const theme = atob(decodeURIComponent(window.location.search.replace(/^[?]c=/, '')));
+				this.service.fromExternal(theme);
+			}, 100);
+		}
+	}
 
-  constructor(private el: ElementRef, private zone: NgZone,
-    private snackbar: MatSnackBar, private dialog: MatDialog,
-    public service: ThemeService
-  ) {
-    if (window.location.search) {
-      setTimeout(() => {
-        const theme = atob(decodeURIComponent(window.location.search.replace(/^[?]c=/, '')));
-        this.service.fromExternal(theme);
-      }, 100);
-    }
-  }
+	onReady() {
+		this.ready.next(true);
+	}
 
-  onReady() {
-    this.ready.next(true);
-  }
+	showSource(yes: boolean) {
+		this.showingSource = yes;
+	}
 
-  showSource(yes: boolean) {
-    this.showingSource = yes;
-  }
+	showCredits() {
+		this.dialog.open(CreditsComponent, {
+			width: '500px'
+		});
+	}
 
-  showCredits() {
-    this.dialog.open(CreditsComponent, {
-      width: '500px',
-    });
-  }
+	copy(title: string, val: string) {
+		const el = document.createElement('textarea');
+		el.value = val;
+		document.body.appendChild(el);
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
+		this.snackbar.open(`Successfully copied ${title} to clipboard!`, 'dismiss', {
+			duration: 3000
+		});
+	}
 
-  copy(title: string, val: string) {
-    const el = document.createElement('textarea');
-    el.value = val;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    this.snackbar.open(`Successfully copied ${title} to clipboard!`, 'dismiss', {
-      duration: 3000
-    });
-  }
+	exportCSS() {
+		this.copy('css', this.css);
+	}
 
-  exportCSS() {
-    this.copy('css', this.css);
-  }
+	exportSCSS() {
+		this.copy('Angular scss', this.source);
+	}
 
-  exportSCSS() {
-    this.copy('Angular scss', this.source);
-  }
+	makeLink() {
+		let link = window.location.toString().replace(/[#?].*$/g, '');
+		link = `${link}?c=${btoa(this.service.toExternal())}`;
+		this.copy('link', link);
+	}
 
-  makeLink() {
-    let link = window.location.toString().replace(/[#?].*$/g, '');
-    link = `${link}?c=${btoa(this.service.toExternal())}`;
-    this.copy('link', link);
-  }
+	ngOnInit() {
+		this.ready
+			.pipe(
+				take(1),
+				switchMap(x => this.service.$theme),
+				debounceTime(100)
+			)
+			.subscribe(x => {
+				this.updateTheme(x);
+				setTimeout(() => (this.isReady = true), 1000);
+			});
 
-  ngOnInit() {
-    this.ready
-      .pipe(
-        take(1),
-        switchMap(x => this.service.$theme),
-        debounceTime(100)
-      )
-      .subscribe(x => {
-        this.updateTheme(x);
-        setTimeout(() => this.isReady = true, 1000);
-      });
+		window.addEventListener('message', ev => {
+			if (ev.data && ev.data.iconsDone) {
+				console.log('Got It!', ev);
+			}
+		});
+	}
 
-    window.addEventListener('message', (ev) => {
-      if (ev.data && ev.data.iconsDone) {
-        console.log('Got It!', ev);
-      }
-    });
-  }
+	updateTheme(theme: Theme) {
+		if (!theme.palette || !theme.fonts) {
+			return;
+		}
 
-  updateTheme(theme: Theme) {
+		this.source = this.service.getTemplate(theme);
 
-    if (!theme.palette || !theme.fonts) {
-      return;
-    }
+		const iframe = (this.el.nativeElement as HTMLElement).querySelector('iframe');
+		const body = iframe.contentDocument.body;
 
-    this.source = this.service.getTemplate(theme);
+		this.sourcePretty = highlight(this.source);
 
-    const iframe = (this.el.nativeElement as HTMLElement).querySelector('iframe');
-    const body = iframe.contentDocument.body;
+		this.zone.runOutsideAngular(() => {
+			window.postMessage({ icons: theme.icons }, window.location.toString());
+			this.service
+				.compileScssTheme(this.source)
+				.then(text => {
+					this.css = text;
+					if (
+						body.childNodes &&
+						body.childNodes.item(0) &&
+						(body.childNodes.item(0) as HTMLElement).tagName &&
+						(body.childNodes.item(0) as HTMLElement).tagName.toLowerCase() === 'style'
+					) {
+						body.removeChild(body.childNodes.item(0));
+					}
 
-    this.sourcePretty = highlight(this.source);
-
-
-    this.zone.runOutsideAngular(() => {
-      window.postMessage({ icons: theme.icons }, window.location.toString());
-      this.service.compileScssTheme(this.source).then(text => {
-        this.css = text;
-        if (body.childNodes && body.childNodes.item(0) &&
-          (body.childNodes.item(0) as HTMLElement).tagName &&
-          (body.childNodes.item(0) as HTMLElement).tagName.toLowerCase() === 'style') {
-          body.removeChild(body.childNodes.item(0));
-        }
-
-        const style = iframe.contentDocument.createElement('style');
-        style.type = 'text/css';
-        style.textContent = text;
-        body.insertBefore(style, body.childNodes.item(0));
-      }).catch(err => {
-        console.error(err);
-      });
-    });
-  }
+					const style = iframe.contentDocument.createElement('style');
+					style.textContent = text;
+					body.insertBefore(style, body.childNodes.item(0));
+				})
+				.catch(err => {
+					console.error(err);
+				});
+		});
+	}
 }
